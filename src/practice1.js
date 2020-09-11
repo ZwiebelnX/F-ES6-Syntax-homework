1e006ed75af44642b97d3847715f674838f72887
Chen Sicong <sicong.chen@163.com>
2020-09-11 18:05:00 +0800

    [ChenSicong]feat: finish practice1 and practice2

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,14 @@
-const parseData
+const parseData = (input) => {
+    const result = []
+    input.data.forEach(array => {
+        let columnObj = {}
+        let arrayIndex = 0
+        input.column.forEach(column => {
+            columnObj[column.name] = array[arrayIndex]
+            arrayIndex++
+        })
+        result.push(columnObj)
+    })
+    return result;
+}
 export { parseData };
