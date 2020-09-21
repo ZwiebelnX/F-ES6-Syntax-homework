3e184b9425d5db3c9013d114e698e43e9a0417c0
kevindongzg <dongkevin94@gmail.com>
2020-09-21 20:35:45 +0800

    add TODO

--- src/practice1.js
+++ src/practice1.js
@@ -1,7 +1,10 @@
+// TODO 使用解构的方式取出data与column
 const parseData = (input) => {
+    // TODO 不需要声明一个[], 可以用map直接进行list转化
     const result = []
     input.data.forEach(array => {
         let columnObj = {}
+        // 不需要单独神明index, 可以查一下forEach内函数的第二个参数就是index
         let arrayIndex = 0
         input.column.forEach(column => {
             columnObj[column.name] = array[arrayIndex]
--- src/practice2.js
+++ src/practice2.js
@@ -1,3 +1,4 @@
+// TODO 逻辑上没有问题，但是嵌套层太多，可以思考有没有办法可以减少嵌套
 const inject = (items, sections) => {
     const result = []
     items.forEach((item, arrayIndex) => {
